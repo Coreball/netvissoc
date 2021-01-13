c9bad652b9b610d5dfb1ef460dff7e6c3a275f11
Changyuan Lin <coreballium@outlook.com>
2021-01-12 23:32:41 -0700

    Functions to add nodes and edges from data

--- src/vis/vis.js
+++ src/vis/vis.js
@@ -1,22 +1,7 @@
 /* eslint-disable no-undef */
 
-// create an array with nodes
-const nodes = new vis.DataSet([
-  { id: 1, label: 'Node 1' },
-  { id: 2, label: 'Node 2' },
-  { id: 3, label: 'Node 3' },
-  { id: 4, label: 'Node 4' },
-  { id: 5, label: 'Node 5' },
-])
-
-// create an array with edges
-const edges = new vis.DataSet([
-  { from: 1, to: 3 },
-  { from: 1, to: 2 },
-  { from: 2, to: 4 },
-  { from: 2, to: 5 },
-  { from: 3, to: 3 },
-])
+const nodes = new vis.DataSet()
+const edges = new vis.DataSet()
 
 // create a network
 const container = this.document.querySelector('#network')
@@ -24,17 +9,46 @@ const data = {
   nodes: nodes,
   edges: edges,
 }
-const options = {}
+const options = {
+  nodes: {
+    shape: 'circle',
+  },
+}
 const network = new vis.Network(container, data, options)
 
-const fileSelect = this.document.querySelector('#fileSelect')
+function colorType(relationType) {
+  return 'grey'
+}
+
+function addToNodes(people) {
+  nodes.add(people.map(person => ({
+    id: person.name,
+    label: person.name,
+    title: person.notes,
+  })))
+}
+
+function addToEdges(people) {
+  edges.add(people.map(person =>
+    person.connections.map(connection =>
+      connection.relations.map(relation => ({
+        from: person.name,
+        to: connection.name,
+        arrows: 'to',
+        color: colorType(relation.type),
+        title: relation.notes,
+      }))
+    ).flat()
+  ).flat())
+}
 
 async function loadFiles(fileList) {
   const files = [...fileList]
   const filesText = await Promise.all(files.map(file => file.text()))
   const filesJson = filesText.map(file => JSON.parse(file))
-  // eslint-disable-next-line no-console
-  console.log(filesJson)
+  addToNodes(filesJson)
+  addToEdges(filesJson)
 }
 
+const fileSelect = this.document.querySelector('#fileSelect')
 fileSelect.addEventListener('change', () => loadFiles(fileSelect.files))
