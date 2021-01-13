7fae760ef426a041448a8d1ac330523187d0bdee
Changyuan Lin <coreballium@outlook.com>
2021-01-12 21:41:12 -0700

    Add vis-network ex

--- src/vis/vis.js
+++ src/vis/vis.js
@@ -1,3 +1,32 @@
+/* eslint-disable no-undef */
+
+// create an array with nodes
+const nodes = new vis.DataSet([
+  { id: 1, label: 'Node 1' },
+  { id: 2, label: 'Node 2' },
+  { id: 3, label: 'Node 3' },
+  { id: 4, label: 'Node 4' },
+  { id: 5, label: 'Node 5' },
+])
+
+// create an array with edges
+const edges = new vis.DataSet([
+  { from: 1, to: 3 },
+  { from: 1, to: 2 },
+  { from: 2, to: 4 },
+  { from: 2, to: 5 },
+  { from: 3, to: 3 },
+])
+
+// create a network
+const container = this.document.querySelector('#network')
+const data = {
+  nodes: nodes,
+  edges: edges,
+}
+const options = {}
+const network = new vis.Network(container, data, options)
+
 const fileSelect = this.document.querySelector('#fileSelect')
 
 async function loadFiles(fileList) {
